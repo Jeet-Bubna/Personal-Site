81a0b06c58ae0c7c0e25bc3a80c8ba05f5050555
Jeet Bubna <bubnajeet@gmail.com>
2026-04-01 18:36:21 +0530

    Navbar: Added navbar by grid

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,27 @@
 function Navbar() {
   return (
-    <div > 
-        Navigate
-        <nav>
-            <ul className='list-none px-10'>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#programming'>Programming</a></li>
-                <li><a href='#blog'>Blog</a></li>
-                <li><a href='#other'>Other</a></li>
-                <li><a href='#contact'>Contact</a></li>
-            </ul>
-        </nav>
+    <div className="my-auto py-2.5">
+      <nav className="rounded-xl text-2xl">
+        <ul className="list-none px-10 py-4 space-y-5">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#programming">Programming</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#other">Other</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      Home
+    <div className="">
+      <div className="grid grid-cols-[250px_1fr] h-screen ">
+        <Navbar />
+        <h1>Main Content</h1>
+      </div>
     </div>
   );
 }
