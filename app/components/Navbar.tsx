3f93cda2e79d2c34969ac7058238abcc04f54183
Jeet Bubna <bubnajeet@gmail.com>
2026-04-01 19:07:19 +0530

    Navbar: Added key to li

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,20 @@
 function Navbar() {
-
   const listItems = {
-    "Home":"#home",
-    "Programming":"#programming",
-    "Blog":"#blog",
-    "Other":"#other",
-    "Contact":"#contact"
+    Home: "#home",
+    Programming: "#programming",
+    Blog: "#blog",
+    Other: "#other",
+    Contact: "#contact",
   };
-  
+
   return (
     <div className="my-auto py-2.5 font-navbar">
       <nav className="rounded-xl text-2xl">
         <ul className="list-none px-10 py-4 space-y-5">
           {Object.entries(listItems).map(([label, url]) => (
-            <li className="transition-transform hover:text-green-200 hover:scale-150 origin-left ">
-            <a href={url}>{label}</a>
-          </li>
+            <li className="transition-transform hover:text-green-200 hover:scale-150 origin-left" key={label}>
+              <a href={url}>{label}</a>
+            </li>
           ))}
         </ul>
       </nav>
