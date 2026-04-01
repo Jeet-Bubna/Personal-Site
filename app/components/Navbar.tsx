09a3f709e9126b938867d96753a4ad74d6736ec3
Jeet Bubna <bubnajeet@gmail.com>
2026-04-01 14:34:28 +0530

    Theme: Created Navbar, and got themes working using next-themes

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <div > 
+        Navigate
+        <nav>
+            <ul className='list-none px-10'>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#programming'>Programming</a></li>
+                <li><a href='#blog'>Blog</a></li>
+                <li><a href='#other'>Other</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/components/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+
+function ThemeProvider({children, ...props}:React.ComponentProps<typeof NextThemeProvider>) {
+  return (
+    <NextThemeProvider {...props}>{children}</NextThemeProvider>
+  )
+}
+
+export default ThemeProvider
