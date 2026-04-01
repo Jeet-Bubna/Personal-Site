2f22d50331075cb6a20f646c96793ec4095a3489
Jeet Bubna <bubnajeet@gmail.com>
2026-04-01 19:03:01 +0530

    Navbar: Beautifed navbar and added hover effect, refactored to a list

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,22 @@
 function Navbar() {
+
+  const listItems = {
+    "Home":"#home",
+    "Programming":"#programming",
+    "Blog":"#blog",
+    "Other":"#other",
+    "Contact":"#contact"
+  };
+  
   return (
     <div className="my-auto py-2.5 font-navbar">
       <nav className="rounded-xl text-2xl">
         <ul className="list-none px-10 py-4 space-y-5">
-          <li>
-            <a href="#home">Home</a>
+          {Object.entries(listItems).map(([label, url]) => (
+            <li className="transition-transform hover:text-green-200 hover:scale-150 origin-left ">
+            <a href={url}>{label}</a>
           </li>
-          <li>
-            <a href="#programming">Programming</a>
-          </li>
-          <li>
-            <a href="#blog">Blog</a>
-          </li>
-          <li>
-            <a href="#other">Other</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+          ))}
         </ul>
       </nav>
     </div>
